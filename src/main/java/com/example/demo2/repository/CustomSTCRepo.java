8d777ccd37efd9004720c606705be5b1a4c5ccc6
Engin ZEYBEKOGLU <32881853+ezeybekoglu@users.noreply.github.com>
2020-09-13 17:28:54 +0300

    Update CustomSTCRepo.java

--- src/main/java/com/example/demo2/repository/CustomSTCRepo.java
+++ src/main/java/com/example/demo2/repository/CustomSTCRepo.java
@@ -4,4 +4,5 @@ import com.example.demo2.model.CustomSTC;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomSTCRepo extends CrudRepository<CustomSTC, Long> {
+  boolean existsByStudent_idAndCourse_id(Long course,Long student);
 }
