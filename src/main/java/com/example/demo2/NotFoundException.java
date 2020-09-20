4916d53d3df706177ab4447d1012865c88ba928c
EnginZeybekoglu <enginzeybekoglu@gmail.com>
2020-09-20 15:02:22 +0300

    20 1

--- src/main/java/com/example/demo2/NotFoundException.java
+++ src/main/java/com/example/demo2/NotFoundException.java
@@ -2,6 +2,6 @@ package com.example.demo2;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(Long id) {
-        super("Could not find student " + id);
+        super("Could not find record " + id);
     }
 }
