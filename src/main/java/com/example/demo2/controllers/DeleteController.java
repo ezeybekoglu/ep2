2aed4c0cb09f7df97dbe82f14e431de637d3157f
EnginZeybekoglu <enginzeybekoglu@gmail.com>
2020-09-20 15:05:58 +0300

    20 4

--- src/main/java/com/example/demo2/controllers/DeleteController.java
+++ src/main/java/com/example/demo2/controllers/DeleteController.java
@@ -1,15 +1,22 @@
 package com.example.demo2.controllers;
 
 import com.example.demo2.repository.CourseRepository;
+import com.example.demo2.repository.CustomSTCRepo;
+import com.example.demo2.repository.STCRepository;
 import com.example.demo2.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DeleteController {
+    @Autowired
     private StudentRepository studentRepository;
+    @Autowired
     private CourseRepository courseRepository;
+    @Autowired
+    private CustomSTCRepo customSTCRepo;
 
 
     @DeleteMapping(value = "/student/{id}")
@@ -20,5 +27,10 @@ public class DeleteController {
     @DeleteMapping(value = "/course/{id}")
     public void deleteCourse(@PathVariable Long id) {
         courseRepository.deleteById(id);
+    }
+
+    @DeleteMapping(value = "/cast/{id}")
+    public void deleteCast(@PathVariable Long id) {
+        customSTCRepo.deleteById(id);
     }
 }
