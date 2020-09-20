7753540d38fe0ab66aac74c5316384821485a65c
EnginZeybekoglu <enginzeybekoglu@gmail.com>
2020-09-20 15:10:40 +0300

    20 5

--- src/main/java/com/example/demo2/services/ICSTCService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo2.services;
-
-import com.example.demo2.model.CustomSTC;
-import java.util.List;
-
-public interface ICSTCService {
-    public List<CustomSTC> findAll();
-}
--- src/main/java/com/example/demo2/services/ICourseService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2.services;
-
-import com.example.demo2.model.Course;
-
-import java.util.List;
-
-public interface ICourseService {
-    public List<Course> findAll();
-}
